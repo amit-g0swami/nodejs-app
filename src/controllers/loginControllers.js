f1cce1f50cbf3537749773ad0291df13cdd62482
amit-g0swami <goswamiempire@gmail.com>
2024-01-31 17:26:32 +0530

    minor fix

--- src/controllers/loginControllers.js
+++ src/controllers/loginControllers.js
@@ -4,20 +4,21 @@ const User = require("../models/User");
 module.exports.login = async (req, res) => {
   try {
     const { email, name, createdAs } = req.body;
-    let existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: 'User already exists', user: existingUser });
+    const existingUser = await User.findOne({ email });
+    if (existingUser && existingUser.createdAs === createdAs) {
+      return res.status(201).json({ message: 'User already exists', user: existingUser });
     }
     const newUser = new User({
       email,
       name,
-      createdAs
+      createdAs,
     });
-    await newUser.save();  
-    res.status(201).json({ message: 'User created successfully', user: newUser });
+    await newUser.save();
+    return res.status(201).json({ message: 'User created successfully', user: newUser });
   } catch (error) {
     console.error('Error creating user:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
-};
+}
+  
 
