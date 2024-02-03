b8db0422590466dfa4c03dae22f8b60289e021be
amit-g0swami <goswamiempire@gmail.com>
2024-02-03 17:06:17 +0530

    fix: login process

--- src/controllers/loginControllers.js
+++ src/controllers/loginControllers.js
@@ -1,20 +1,42 @@
 const User = require("../models/User");
 
-
 module.exports.login = async (req, res) => {
   try {
     const { email, name, createdAs } = req.body;
-    const existingUser = await User.findOne({ email });
-    if (existingUser && existingUser.createdAs === createdAs) {
-      return res.status(201).json({ message: 'User already exists', user: existingUser });
+    if(!createdAs || !email || !name) {
+      return res.status(201).json({ message: 'Invalid Credentials', user: null });
     }
-    const newUser = new User({
-      email,
-      name,
-      createdAs,
-    });
-    await newUser.save();
-    return res.status(201).json({ message: 'User created successfully', user: newUser });
+
+    const users = await User.find({ email });
+
+    if (users.length === 0) {
+      const newUser = new User({
+        email,
+        name,
+        createdAs,
+      });
+      await newUser.save();
+      return res.status(201).json({ message: 'User created successfully', user: newUser });
+    }
+
+    if(users.length === 1){
+      if(users.filter(user => user.createdAs !== createdAs)) {
+        const newUser = new User({
+          email,
+          name,
+          createdAs,
+        });
+        await newUser.save();
+        return res.status(201).json({ message: 'User created successfully', user: newUser });
+      }else{
+        return res.status(201).json({ message: 'User already exists', user: users[0] });
+      }
+     }
+
+    if(users.length >= 2){
+      const filteredUser = users.filter(user => user.createdAs === createdAs);
+      return res.status(201).json({ message: 'User already exists', user: filteredUser[0] });
+    }
   } catch (error) {
     console.error('Error creating user:', error);
     res.status(500).json({ message: 'Internal server error' });
